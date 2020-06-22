318e7798b644de23fb5d4580347a57eba035f947
Thielicious <TheDefinitionist@users.noreply.github.com>
2020-06-22 20:11:44 +0200

    Add files via upload

--- demo/index.php
+++ demo/index.php
@@ -11,24 +11,24 @@
 		<h2>cImgur Demo</h2>
 		<p><small>cImgur v1.01 | &copy; 2017 | Thielicious | https://thielicious.github.io</small></p><br>
 		
-		<form action="<?php $_SERVER["PHP_SELF"] ?>" method="POST" enctype="multipart/form-data">
+		<form action="" method="POST" enctype="multipart/form-data">
 			<label>Choose an image to upload</label><br>
 			<input type="file" name="browse">
 			<input type="submit" name="upload" value="Upload">
-		</form>
+		</form><br>
 		
 		<?php
 		
 			$imgur = new cImgur("b9ec36b6c896798"); // <--- enter your own Client-ID by registering at https://api.imgur.com/oauth2/addclient
-			$imgur->setUploadSize(150000);
+			$imgur->setUploadSize(200000);
 		
 			if (isset($_POST["upload"])) {
 				if (isset($_FILES["browse"])) {	
-				
+					echo "test";
 					try {
 						$imgur->upload($_FILES["browse"]);
 						$response = $imgur->data(cImgur::RETURN_OBJECT);
-						echo "<p><span style=color:green>Upload succeeded: <span></p>
+						echo "<p><span style=color:lightgreen>Upload succeeded: <span></p>
 								<img src='".$response->link."'>						
 								<p>Link: <a href=".$response->link.">".$response->link."</a></p>";
 					} catch (Exception $e) {
@@ -40,7 +40,7 @@
 				}
 				
 			} else {
-				echo "<br>Please upload an image.";
+				echo "<p>Please upload an image.</p>";
 			}
 			
 		?>
